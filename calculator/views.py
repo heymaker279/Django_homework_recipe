a5ff3131f1eba0a41aba0d843c1e4bd09a60a6c0
Валеев Ильяс <heymaker279@gmail.com>
2022-06-01 07:53:56 +0500

    initial_commit

---
+++ calculator/views.py
@@ -0,0 +1,59 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+
+def home_page(request):
+    return HttpResponse("Рецепты")
+    # можете добавить свои рецепты ;)
+
+def omlet(request):
+    context = {'recipe': DATA['omlet']}
+    return render(request, 'calculator/index.html', servings(request, context))
+
+
+def pasta(request):
+    context = {'recipe': DATA['pasta']}
+    return render(request, 'calculator/index.html', servings(request, context))
+
+
+def buter(request):
+    context = {'recipe': DATA['buter']}
+    return render(request, 'calculator/index.html', servings(request, context))
+
+
+def servings(request, context):
+    servings = int(request.GET.get('servings', 1))
+    recipe = {}
+    for ingredient, amount in context['recipe'].items():
+        amount *= servings
+        recipe[f'{ingredient}'] = round(amount, 2)
+    context = {'recipe': recipe}
+    return context
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
